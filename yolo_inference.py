a2b7fdf1540bda2370242975eb366ac60fe88437
Lou Brauchli <lou.brauchli@gmail.com>
2025-11-11 14:34:52 +0100

    add training notebook

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO('yolo_training/models/fifth_model/run1/weights/best.pt')
 
-results = model.predict('input_videos_match/Test/wiesendangen_test_clip.mp4', save=True, stream=True, device='mps')
+results = model.predict('input_videos_match/Test/wiesendangen_test_clip_short.mp4', save=True, stream=True, device='mps')
 #print(results[0])
 #print('========================================')
 #for box in results[0].boxes:
