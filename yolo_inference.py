b00ed27914a8c04a5123a30d0b734a8e1b88b70e
Lou Brauchli <lou.brauchli@gmail.com>
2025-11-11 14:13:27 +0100

    add yolo_inference.py file

---
+++ yolo_inference.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+model = YOLO('yolo_training/models/fifth_model/run1/weights/best.pt')
+
+results = model.predict('input_videos_match/Test/wiesendangen_test_clip.mp4', save=True, stream=True, device='mps')
+#print(results[0])
+#print('========================================')
+#for box in results[0].boxes:
+#    print(box)
+
+for i, r in enumerate(results):
+    print(f"Frame {i}")
+    print("========================================")
