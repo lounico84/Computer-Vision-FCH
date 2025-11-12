968dd3235adb66d6b77486c297f8c16f76650194
Lou Brauchli <lou.brauchli@gmail.com>
2025-11-12 18:07:06 +0100

    read fps of video

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -2,13 +2,22 @@ import cv2
 
 def read_video(video_path):
     cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        raise FileNotFoundError(f"Could not open video: {video_path}")
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    if not fps or fps <= 1e-3:
+        fps = 30.0  # Fallback
+
     frames = []
     while True:
         ret, frame = cap.read()
         if not ret:
             break
         frames.append(frame)
-    return frames
+
+    cap.release()
+    return frames, fps
 
 def save_video(output_video_frames, output_video_path):
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
