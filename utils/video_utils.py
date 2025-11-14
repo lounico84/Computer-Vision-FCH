2e685420195934f02f33d78202c07feff3c3026b
Lou Brauchli <lou.brauchli@gmail.com>
2025-11-14 12:38:46 +0100

    added comments

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,18 +1,27 @@
 import cv2
 
+# Read all frames from a video file into a list
 def read_video(video_path):
     cap = cv2.VideoCapture(video_path)
     frames = []
     while True:
         ret, frame = cap.read()
         if not ret:
+            # End of video or read error
             break
         frames.append(frame)
     return frames
 
+# Save a list of frames as a video file
 def save_video(output_video_frames, output_video_path):
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+    # Create video writer
     out = cv2.VideoWriter(output_video_path, fourcc, 60, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
+
+    # Write each frame to the output video
     for frame in output_video_frames:
         out.write(frame)
+
+    # Close file
     out.release()
