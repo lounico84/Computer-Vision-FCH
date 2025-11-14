345af21904bdf4b9f4fc8bd3b623aad29434e368
Lou Brauchli <lou.brauchli@gmail.com>
2025-11-14 12:37:03 +0100

    added comments

--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -1,9 +1,12 @@
+# Calculate center point (x,y) of bbox
 def get_center_of_bbox(bbox):
     x1, y1, x2, y2 = bbox
     return int((x1+x2)/2), int((y1+y2)/2)
 
+# Return the width of bbox
 def get_bbox_width(bbox):
     return bbox[2]-bbox[0]
 
+# Calculate the euclidean distance between two points p1 and p2
 def measure_distance(p1, p2):
     return ((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)**0.5
